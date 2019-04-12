966aebffa0f33a8dde86a5aae2707d95aaeeade3
leejongcheal <aksndk123@naver.com>
2019-04-12 19:23:58 +0900

    commit 2

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -9,6 +9,39 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
+    
+    var items: [ChecklistItem]
+    
+    required init?(coder aDecoder: NSCoder) {
+        items = [ChecklistItem]()
+        
+        let row0item = ChecklistItem()
+        row0item.text = "Walk the dog"
+        row0item.checked = false
+        items.append(row0item)
+        
+        let row1item = ChecklistItem()
+        row1item.text = "Brush my teeth"
+        row1item.checked = false
+        items.append(row1item)
+        
+        let row2item = ChecklistItem()
+        row2item.text = "Learn iOS development"
+        row2item.checked = false
+        items.append(row2item)
+        
+        let row3item = ChecklistItem()
+        row3item.text = "Soccer practice"
+        row3item.checked = false
+        items.append(row3item)
+        
+        let row4item = ChecklistItem()
+        row4item.text = "Eat ice cream"
+        row4item.checked = false
+        items.append(row4item)
+        
+        super.init(coder: aDecoder)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,7 +68,7 @@ class ChecklistViewController: UITableViewController {
             label.text = "Brush my teeth"
         }
         else if indexPath.row % 5 == 2 {
-            label.text = "Learn iOS developement"
+            label.text = "Learn iOS development"
         }
         else if indexPath.row % 5 == 3 {
             label.text = "Soccer practice"
@@ -44,6 +77,19 @@ class ChecklistViewController: UITableViewController {
             label.text = "Eat ice cream"
         }
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 
---
+++ Checklists/Checklistitem.swift
@@ -0,0 +1,18 @@
+//
+//  Checklistitem.swift
+//  Checklists
+//
+//  Created by 캡디 on 12/04/2019.
+//  Copyright © 2019 hongjehong. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+    /* For toggling */
+    func toggleChecked() {
+        checked = !checked
+    }
+}
