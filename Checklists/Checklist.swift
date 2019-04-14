456a24d92eff310b5ef605192e62b2cfb9b5b433
leejongcheal <aksndk123@naver.com>
2019-04-14 19:14:05 +0900

    commit

---
+++ Checklists/Checklist.swift
@@ -0,0 +1,18 @@
+//
+//  Checklist.swift
+//  Checklists
+//
+//  Created by 캡디 on 14/04/2019.
+//  Copyright © 2019 hongjehong. All rights reserved.
+//
+
+import UIKit
+
+class Checklist: NSObject {
+    var name = ""
+    
+    init(name: String) {
+        self.name = name
+        super.init()
+    }
+}
